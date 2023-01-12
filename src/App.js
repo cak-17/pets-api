f520d4b702316ac845ae4aab9ed77fbfc75f4f41
cak-17 <98436543+cak-17@users.noreply.github.com>
2023-01-12 11:17:56 +1100

    pets-api first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react' 
+import { Amplify, API } from 'aws-amplify';
+import config from './aws-exports';
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import './App.css';
 
-function App() {
+Amplify.configure(config)
+
+function App({signOut, user}) {
+  const [petName, setPetName] = useState('');
+  const [petType, setPetType] = useState('');
+  const [pets, setPets] = useState([]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    API.post("petsapi", "/pets", {
+      body: {
+        name: petName,
+        type: petType,
+      }
+    }).then(() => {
+      setPets([{name: petName, type: petType}, ...pets])
+    })
+  }
+  useEffect(() => {
+    API.get("petsapi", "/pets/name")
+    .then(petResponse => setPets([...pets, ...petResponse]))
+  },[])
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+      <Heading color="white" level={1}>Hello {user.username}!</Heading>
+      <form onSubmit={handleSubmit}>
+      <input value={petName} placeholder="fido" onChange={(e) => setPetName(e.target.value)}/>
+      <input value={petType} placeholder="dog" onChange={(e) => setPetType(e.target.value)}/>
+      <button>Add</button>
+      </form>
+      <ul>
+        {pets.map(pet => <li key={pet.name}>{pet.name}</li>)}
+      </ul>
+      <Button variation="primary" onClick={signOut}>Sign out</Button>
+    </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
